897ba0a7fd8e404c76b029e7b31482dfde2e5680
joergboe <joergboe@de.ibm.com>
2019-05-21 11:11:29 +0200

    Correct start up sequence of test suite

--- tests/frameworktests/tests/CollectionMail/Runtests/TestSuite.sh
+++ tests/frameworktests/tests/CollectionMail/Runtests/TestSuite.sh
@@ -5,3 +5,8 @@ fi
 import mailtools.sh
 
 setVar 'TTPR_timeout' 480
+
+#Make sure instance and domain is running
+PREPS='cleanUpInstAndDomainAtStart mkDomain startDomain mkInst startInst'
+FINS='cleanUpInstAndDomainAtStop'
+
