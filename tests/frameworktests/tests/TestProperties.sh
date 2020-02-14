a6c5e05f47a1c9430d4f63a11c567a62541203fb
joergboe <joergboe@de.ibm.com>
2020-02-14 13:56:11 +0100

    Encryp mail user credentials

--- tests/frameworktests/tests/TestProperties.sh
+++ tests/frameworktests/tests/TestProperties.sh
@@ -9,9 +9,4 @@ setVar 'TTPR_streamsxMailToolkit' "$TTRO_inputDir/../../../com.ibm.streamsx.mail
 setVar 'TT_toolkitPath' "${TTPR_streamsxMailToolkit}" #consider more than one tk...
 
 #Mail configuration
-setVar 'TTPR_mailDomain' 'strvm2.net1'
-setVar 'TTPR_mailServer' 'strvm2.net1'
-setVar 'TTPR_mailUser1' 'mailuser1'
-setVar 'TTPR_mailUser2' 'mailuser2'
-setVar 'TTPR_mailPass1' 'streams1'
-setVar 'TTPR_mailPass2' 'streams1'
+setVar 'TTPR_envionmentFile' "$TTRO_inputDir/mailenv.enc"
